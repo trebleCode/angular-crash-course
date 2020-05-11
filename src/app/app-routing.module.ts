a4b0835bea3b8bbc3290a902ad02ce19cf5984d9
Scott Sweeney <scott.sweeney@live.com>
2020-05-11 16:14:49 -0400

    Initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { AboutComponent } from './components/pages/about/about.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '', component: TodosComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // tslint:disable-next-line:no-inferrable-types
+  name: string = 'Scott';
+}
